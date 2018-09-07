1afce11435850b06ec7c9ffd55ee17e5fb5cbb53
roxy991 <golf3501991@htmail.com>
2018-09-07 14:56:47 +0700

    W04P09

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,6 +7,10 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.Read();
+            Console.WriteLine("You entered: " + str);
         }
     }
 }
