fe3df610635a1b58b8fcd9a05bd5f970df974b6f
roxy991 <golf3501991@htmail.com>
2018-09-07 14:19:23 +0700

    W04P05

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,6 +7,13 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            int n = 123456789;
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);
         }
     }
 }
