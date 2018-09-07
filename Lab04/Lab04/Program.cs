d02c6006f3723566bf092fb841a3a9a505d3afff
roxy991 <golf3501991@htmail.com>
2018-09-07 14:24:16 +0700

    W04P07

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,6 +7,12 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            const double i = 123.456789d;
+            Console.WriteLine("{0:F1}", i);
+            Console.WriteLine("{0:F2}", i);
+            Console.WriteLine("{0:F3}", i);
+            Console.WriteLine("{0:F4}", i);
+            Console.WriteLine("{0:F5}", i);
         }
     }
 }
