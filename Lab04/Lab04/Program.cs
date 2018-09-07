f673de6093f482deba4c621f8ca99d639eadf78c
roxy991 <golf3501991@htmail.com>
2018-09-07 14:59:14 +0700

    W04P10

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -7,6 +7,10 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            string str;
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
         }
     }
 }
